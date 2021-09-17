c0a309f79e928e09d1d316c0dcb9b7d28a536bdd
zhangjianjun <wusen@126.com>
2021-09-17 10:19:14 +0800

    Add parsing excel function

--- taskhandler/download.go
+++ taskhandler/download.go
@@ -36,25 +36,25 @@ func DownloadFile() error {
 		ExcelOwner: excelOwner, ExcelRepo: excelRepo, AccessToken: accessToken}
 	stdErr := DownloadExcel(stdInfo)
 	if stdErr != nil {
-		logs.Error("stdErr: ", stdErr)
-		return stdErr
-	}
-	stdFileName := filepath.Join(stdInfo.LocalDir, stdInfo.RemoteFileName)
-	excelData := ExcelParse(stdFileName)
-	if len(excelData) > 0 {
-		ProcStdData(excelData, accessToken)
+		logs.Error("DownloadExcel, stdErr: ", stdErr)
+	} else {
+		stdFileName := filepath.Join(stdInfo.LocalDir, stdInfo.RemoteFileName)
+		excelData := ExcelParse(stdFileName)
+		if len(excelData) > 0 {
+			ProcStdData(excelData, accessToken)
+		}
 	}
 	tutInfo := ExcelFileInfo{LocalDir: dir, RemoteFileName: tutorFilePath,
 		ExcelOwner: excelOwner, ExcelRepo: excelRepo, AccessToken: accessToken}
 	tutErr := DownloadExcel(tutInfo)
 	if tutErr != nil {
 		logs.Error("tutErr: ", tutErr)
-		return tutErr
-	}
-	tutFileName := filepath.Join(tutInfo.LocalDir, tutInfo.RemoteFileName)
-	excelData = ExcelParse(tutFileName)
-	if len(excelData) > 0 {
-		ProcTutData(excelData, accessToken)
+	} else {
+		tutFileName := filepath.Join(tutInfo.LocalDir, tutInfo.RemoteFileName)
+		excelData := ExcelParse(tutFileName)
+		if len(excelData) > 0 {
+			ProcTutData(excelData, accessToken)
+		}
 	}
 	return nil
 }
@@ -63,52 +63,59 @@ func DownloadFile() error {
 func ProcStdData(excelData [][]string, accessToken string) {
 	for _, ed := range excelData {
 		su := models.StdUser{}
-		if len(ed) == 3 {
-			status, stErr := strconv.Atoi(ed[2])
-			if stErr != nil {
-				logs.Error("ProcStdData, stErr: ", stErr)
-				status = 1
+		gitUserId := ""
+		emailAddr := ""
+		statusType := 1
+		if len(ed) <= 1 {
+			logs.Error("data err :", ed)
+			continue
+		} else if len(ed) == 2 {
+			gitUserId = ed[0]
+			statusType, _ = strconv.Atoi(ed[1])
+		} else {
+			gitUserId = ed[0]
+			emailAddr = ed[1]
+			statusType, _ = strconv.Atoi(ed[2])
+		}
+		gui := GetGitId(accessToken, gitUserId)
+		if gui.GitId == 0 {
+			logs.Error("GetGitId, Failed to get gitid information: ", gui)
+			continue
+		}
+		su.GitId = gui.GitId
+		su.GitUserId = gitUserId
+		switch statusType {
+		case 3:
+			delErr := models.DelStdUser(&su, "GitId")
+			if delErr != nil {
+				logs.Error("DelStdUser, delErr: ", delErr)
 			}
-			su.GitUserId = ed[0]
-			switch status {
-			case 3:
-				delErr := models.DelStdUser(&su, "GitUserId")
-				if delErr != nil {
-					logs.Error("DelStdUser, delErr: ", delErr)
-				}
-			default:
-				gui := GetGitId(accessToken, ed[0])
-				if gui.GitId == 0 {
-					logs.Error("GetGitId, Failed to get gitid information: ", gui)
-					continue
-				}
+		default:
+			querErr := models.QueryStdUser(&su, "GitId")
+			if su.UserId == 0 {
+				su.EmailAddr = emailAddr
 				su.GitId = gui.GitId
-				querErr := models.QueryStdUser(&su, "GitId")
-				if su.UserId == 0 {
-					su.EmailAddr = ed[1]
-					su.GitId = gui.GitId
-					su.GitUserId = gui.Login
-					su.CreateTime = common.GetCurTime()
-					su.Status = 1
-					su.UserName = gui.Login
-					inNum, inErr := models.InsertStdUser(&su)
-					if inNum > 0 {
-						logs.Info("InsertStdUser success, inNum:", inNum)
-					} else {
-						logs.Error("InsertStdUser failed, inNum: ", inNum, ", inErr", inErr, querErr)
-					}
+				su.GitUserId = gui.Login
+				su.CreateTime = common.GetCurTime()
+				su.Status = 1
+				su.UserName = gui.Login
+				inNum, inErr := models.InsertStdUser(&su)
+				if inNum > 0 {
+					logs.Info("InsertStdUser success, inNum:", inNum)
 				} else {
-					su.EmailAddr = ed[1]
-					su.UpdateTime = common.GetCurTime()
-					su.Status = 1
-					su.GitUserId = gui.Login
-					su.UserName = gui.Login
-					upErr := models.UpdateStdUser(&su, "EmailAddr", "UpdateTime", "Status", "GitUserId")
-					if upErr == nil {
-						logs.Info("UpdateStdUser success, upErr:", upErr)
-					} else {
-						logs.Error("UpdateStdUser failed, upErr: ", upErr)
-					}
+					logs.Error("InsertStdUser failed, inNum: ", inNum, ", inErr", inErr, querErr)
+				}
+			} else {
+				su.EmailAddr = emailAddr
+				su.UpdateTime = common.GetCurTime()
+				su.Status = 1
+				su.GitUserId = gui.Login
+				su.UserName = gui.Login
+				upErr := models.UpdateStdUser(&su, "EmailAddr", "UpdateTime", "Status", "GitUserId")
+				if upErr == nil {
+					logs.Info("UpdateStdUser success, upErr:", upErr)
+				} else {
+					logs.Error("UpdateStdUser failed, upErr: ", upErr)
 				}
 			}
 		}
@@ -119,52 +126,59 @@ func ProcStdData(excelData [][]string, accessToken string) {
 func ProcTutData(excelData [][]string, accessToken string) {
 	for _, ed := range excelData {
 		tu := models.TutUser{}
-		if len(ed) == 3 {
-			status, stErr := strconv.Atoi(ed[2])
-			if stErr != nil {
-				logs.Error("ProcTutData, stErr: ", stErr)
-				status = 1
+		gitUserId := ""
+		emailAddr := ""
+		statusType := 1
+		if len(ed) <= 1 {
+			logs.Error("data err :", ed)
+			continue
+		} else if len(ed) == 2 {
+			gitUserId = ed[0]
+			statusType, _ = strconv.Atoi(ed[1])
+		} else {
+			gitUserId = ed[0]
+			emailAddr = ed[1]
+			statusType, _ = strconv.Atoi(ed[2])
+		}
+		tu.GitUserId = gitUserId
+		gui := GetGitId(accessToken, gitUserId)
+		if gui.GitId == 0 {
+			logs.Error("GetGitId, Failed to get gitid information: ", gui)
+			continue
+		}
+		tu.GitId = gui.GitId
+		switch statusType {
+		case 3:
+			delErr := models.DelTutUser(&tu, "GitId")
+			if delErr != nil {
+				logs.Error("DelTutUser, delErr: ", delErr)
 			}
-			tu.GitUserId = ed[0]
-			switch status {
-			case 3:
-				delErr := models.DelTutUser(&tu, "GitUserId")
-				if delErr != nil {
-					logs.Error("DelTutUser, delErr: ", delErr)
-				}
-			default:
-				gui := GetGitId(accessToken, ed[0])
-				if gui.GitId == 0 {
-					logs.Error("GetGitId, Failed to get gitid information: ", gui)
-					continue
-				}
+		default:
+			querErr := models.QueryTutUser(&tu, "GitId")
+			if tu.UserId == 0 {
+				tu.EmailAddr = emailAddr
 				tu.GitId = gui.GitId
-				querErr := models.QueryTutUser(&tu, "GitId")
-				if tu.UserId == 0 {
-					tu.EmailAddr = ed[1]
-					tu.GitId = gui.GitId
-					tu.GitUserId = ed[0]
-					tu.CreateTime = common.GetCurTime()
-					tu.Status = 1
-					tu.UserName = gui.Login
-					inNum, inErr := models.InsertTutUser(&tu)
-					if inNum > 0 {
-						logs.Info("InsertTutUser success, inNum:", inNum)
-					} else {
-						logs.Error("InsertTutUser failed, inNum: ", inNum, ", inErr", inErr, querErr)
-					}
+				tu.GitUserId = gui.Login
+				tu.CreateTime = common.GetCurTime()
+				tu.Status = 1
+				tu.UserName = gui.Login
+				inNum, inErr := models.InsertTutUser(&tu)
+				if inNum > 0 {
+					logs.Info("InsertTutUser success, inNum:", inNum)
 				} else {
-					tu.EmailAddr = ed[1]
-					tu.UpdateTime = common.GetCurTime()
-					tu.Status = 1
-					tu.GitUserId = gui.Login
-					tu.UserName = gui.Login
-					upErr := models.UpdateTutUser(&tu, "EmailAddr", "UpdateTime", "Status", "GitUserId")
-					if upErr == nil {
-						logs.Info("UpdateTutUser success, upErr:", upErr)
-					} else {
-						logs.Error("UpdateTutUser failed, upErr: ", upErr)
-					}
+					logs.Error("InsertTutUser failed, inNum: ", inNum, ", inErr", inErr, querErr)
+				}
+			} else {
+				tu.EmailAddr = emailAddr
+				tu.UpdateTime = common.GetCurTime()
+				tu.Status = 1
+				tu.GitUserId = gui.Login
+				tu.UserName = gui.Login
+				upErr := models.UpdateTutUser(&tu, "EmailAddr", "UpdateTime", "Status", "GitUserId")
+				if upErr == nil {
+					logs.Info("UpdateTutUser success, upErr:", upErr)
+				} else {
+					logs.Error("UpdateTutUser failed, upErr: ", upErr)
 				}
 			}
 		}
@@ -201,10 +215,11 @@ func DownloadExcel(efi ExcelFileInfo) error {
 	excePath := beego.AppConfig.String("excel::excel_path")
 	remotePath := fmt.Sprintf("%v/%v", excePath, efi.RemoteFileName)
 	gitUrl := fmt.Sprintf("https://gitee.com/api/v5/repos/%v/%v/contents/%v?access_token=%v",
-		efi.ExcelOwner, efi.ExcelOwner, remotePath, efi.AccessToken)
+		efi.ExcelOwner, efi.ExcelRepo, remotePath, efi.AccessToken)
+	logs.Info("DownloadExcel, gitUrl: ", gitUrl)
 	resp, err := http.Get(gitUrl)
 	if err != nil {
-		logs.Error("get error, url:", gitUrl, "error: ", err)
+		logs.Error("DownloadExcel, error: ", err)
 		return err
 	}
 	defer resp.Body.Close()
